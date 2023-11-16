e64db60220425371c65cb125738f25273c864cc6
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-15 16:40:58 -0800

    Created a simple layout

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-red-600 w-screen h-screen flex justify-center items-center font-bold text-3xl">
+      Home
+    </div>
+  );
+};
+
+export default Home;
