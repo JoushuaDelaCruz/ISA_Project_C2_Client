33049aae145ce520fc0c4e83d23cd922036b8674
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-17 15:48:04 -0800

    Created a background class in index.css to store the background class

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,8 +3,8 @@ import Navbar from "./Components/Nav";
 
 const Home = () => {
   return (
-    <div className="bg-gradient-to-t from-spanish-orange via-spanish-orange to-carmine ">
-      <Navbar />
+    <div className="background">
+      <Navbar pageNum={1} />
       <div className="text-red-600 w-screen h-screen flex justify-center items-center font-bold text-3xl">
         Home Content
       </div>
