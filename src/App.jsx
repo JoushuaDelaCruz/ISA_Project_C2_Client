529de922e9514cd0f35e54ba93310b70d97d2bac
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-19 16:37:54 -0800

    Created the admin and storing of user

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,32 @@ import {
 import Teams from "./Pages/Teams";
 import CreateRoom from "./Pages/CreateRoom";
 import Admin from "./Pages/Admin";
+import { useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  const adminLoader = async () => {
+    if (user && user.admin) {
+      return null;
+    } else {
+      window.location.href = "/";
+      return null;
+    }
+  };
+
   const routes = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/">
-        <Route index element={<Home />} />
+        <Route index element={<Home user={user} />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/create-room" element={<CreateRoom />} />
-        <Route path="/auth" exact element={<Authenticate />} />
-        <Route path="/admin" exact element={<Admin />} />
+        <Route
+          path="/auth"
+          exact
+          element={<Authenticate setUser={setUser} />}
+        />
+        <Route loader={adminLoader} path="/admin" exact element={<Admin />} />
       </Route>
     )
   );
