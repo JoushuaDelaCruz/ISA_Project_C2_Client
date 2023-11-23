d90037cd12410161acda7a1f87c0fde0e6ec130b
JASON_LUI <Jason53lui@gmail.com>
2023-11-23 01:56:48 -0800

    tried to test the routes, but cant find the correct URL

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Nav";
+import useRequest from "./Hooks/useRequest";
 
 const Home = ({ user }) => {
+  const [characterContext, setCharacterContext] = useState("");
+  const { getContextToStory } = useRequest(); 
+
+  const handleGetStory = async () => {
+    try {
+      const response = await getContextToStory(characterContext);
+      console.log(response); 
+    } catch (error) {
+      console.error("Error fetching story:", error);
+    }
+  };
+
   return (
     <div className="background">
       <Navbar pageNum={1} user={user} />
       <div className="w-screen h-screen flex flex-col items-center font-bold text-3xl">
-      <div className="mb-4 pt-72 w-97 flex justify-center text-center pr-8">
-        Telore crafts captivating fantasy backstories from your character context using artificial intelligence and storytelling magic. Try it out!
+        <div className="mb-4 pt-72 w-97 flex justify-center text-center pr-8">
+          Telore crafts captivating fantasy backstories from your character context using artificial intelligence and storytelling magic. Try it out!
         </div>
         <div className="p-4 rounded-lg flex items-center">
           <input
             type="text"
             className="w-97 h-10 border-2 border-gray-400 rounded-l p-2"
             placeholder="Enter your character's context here..."
+            value={characterContext}
+            onChange={(e) => setCharacterContext(e.target.value)}
           />
-          <button className="bg-midnight-green text-white rounded-lg m-2">
+          <button className="bg-midnight-green text-white rounded-lg m-2" onClick={handleGetStory}>
             <i className="fa-solid fa-arrow-right-to-bracket text-2xl p-1.5 pr-2"></i>
           </button>
         </div>
--- src/Pages/Hooks/useRequest.jsx
+++ src/Pages/Hooks/useRequest.jsx
@@ -17,12 +17,16 @@ const useRequest = () => {
   };
 
   const getRequest = async (endpoint, data = undefined) => {
-    const url = urlConstructor(endpoint);
-    let response = await fetch(url, getConfig("GET", data));
+    const url = new URL(urlConstructor(endpoint));
+  
+    if (data) {
+      Object.keys(data).forEach(key => url.searchParams.append(key, data[key]));
+    }
+  
+    let response = await fetch(url, getConfig("GET", undefined));
     response = await response.json();
     return response;
   };
-
   const postRequest = async (endpoint, data = undefined) => {
     const url = urlConstructor(endpoint);
     let response = await fetch(url, getConfig("POST", data));
@@ -59,12 +63,48 @@ const useRequest = () => {
     return response;
   };
 
+  const generateTokens = async (characterContext) => {
+    try {
+      const tokensEndpoint = `/model/GenerateTokens`;
+      const tokensResponse = await getRequest(tokensEndpoint, characterContext);
+      console.log(tokensResponse);
+      return tokensResponse;
+    } catch (error) {
+      console.error("Error generating tokens:", error);
+      throw error;
+    }
+  };
+  
+  const generateStory = async (tokens) => {
+    try {;
+      const storyEndpoint = `/open-ai/GenerateStory`;
+      const storyResponse = await getRequest(storyEndpoint, tokens);
+      console.log(storyResponse);
+      return storyResponse;
+    } catch (error) {
+      console.error("Error generating story:", error);
+      throw error;
+    }
+  };
+
+  const getContextToStory = async (characterContext) => {
+    try {
+      const tokensResponse = await generateTokens(characterContext);
+      const storyResponse = await generateStory(tokensResponse);
+      return storyResponse;
+    } catch (error) {
+      console.error("Error converting context to story:", error);
+      throw error;
+    }
+  };
+
   return {
     getRequest,
     postRequest,
     logInRequest,
     logOutRequest,
     signUpRequest,
+    getContextToStory,
   };
 };
 
