c9be84e6118dc9af0120267896535994721ab17d
JASON_LUI <Jason53lui@gmail.com>
2023-11-25 12:56:50 -0800

    admin tailwind

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -58,6 +58,9 @@ const Admin = () => {
     <main className="background flex flex-col gap-5">
       <Navbar pageNum={4} />
       <section className="flex h-full flex-1 w-full px-7 pb-5 items-center flex-col gap-3">
+      <section className="bg-white/70 font-bold rounded-md w-full max-w-5xl p-3 text-center uppercase text-xl text-midnight-green mt-4">
+        <h3 className="flex justify-center">All User Lores</h3>
+      </section>
         {lores.length > 0 && (
           <div className="max-w-5xl mx-auto">
             <div className="grid grid-cols-3 gap-3">
--- src/Pages/Components/StoryCardAdmin.jsx
+++ src/Pages/Components/StoryCardAdmin.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-const StoryCard = ({ text }) => {
+const StoryCard = ({ text, onDelete }) => {
   return (
-    <div className="py-2 px-4 bg-white/75 text-white font-medium rounded-md min-h-36 shadow-md flex flex-col gap-2">
-      <h4 className="text-midnight-green font-bold text-center sm:text-start">
-        {" "}
-        I am Jason and I am from Canada{" "}
-      </h4>
-      <p className="text-sm text-tyrian-purple">{text}</p>
+    <div className="relative py-2 px-4 bg-white/75 text-white font-medium rounded-md min-h-36 shadow-md flex flex-col gap-2">
+      <button
+        onClick={onDelete}
+        className="absolute top-0 right-0 mt-1 mr-1 text-gray-400 hover:text-red-500 focus:outline-none"
+      >
+        <i className="fa-solid fa-x text-xl pr-1 pt-1"></i>
+      </button>
+      <div>
+        <h4 className="text-midnight-green font-bold text-center sm:text-start">
+          {" "}
+          I am Jason and I am from Canada{" "}
+        </h4>
+      </div>
+      <div>
+        <p className="text-sm text-tyrian-purple">{text}</p>
+      </div>
     </div>
   );
 };
