82cf0e9580daf72c86ec31268d5ae50607afaeef
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-25 15:20:43 -0800

    Remove the static data

--- src/Pages/Components/StoryCard.jsx
+++ src/Pages/Components/StoryCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const StoryCardAdmin = ({ text }) => {
+const StoryCardAdmin = ({ lore }) => {
   return (
     <div className="flex flex-col justify-between bg-white/75 text-white font-medium rounded-md shadow-md flex-grow">
       <div className="py-2 px-4">
         <h4 className="text-midnight-green font-bold text-center sm:text-start">
-          I am Gandalf the Great from The Lord of the Rings and I My companion is Frodo from The Shire
+          {lore.user_text}
         </h4>
-        <p className="text-sm text-tyrian-purple">{text}</p>
+        <p className="text-sm text-tyrian-purple">{lore.story_text}</p>
       </div>
     </div>
   );
