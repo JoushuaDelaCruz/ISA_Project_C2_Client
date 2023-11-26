55afe0ea87a0871bf069933b6b9a33bb8fa97538
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-25 19:44:12 -0800

    Displayed the endpointCard for the endpoints

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Components/Nav";
 import StoryCardAdmin from "./Components/StoryCardAdmin";
 import useRequest from "./Hooks/useRequest";
+import { useLoaderData } from "react-router-dom";
+import EndpointsCard from "./Components/EndpointsCard";
 
 const Admin = ({ user }) => {
   const [loading, setLoading] = useState(true);
   const [lores, setLores] = useState([]);
   const { getRequest } = useRequest();
+  const endpoints = useLoaderData();
 
   useEffect(() => {
     const getAllStories = async () => {
       try {
         const endpoint = "/story/allStories";
         const stories = await getRequest(endpoint);
-        console.log(stories);
         setLores(stories);
         setLoading(false);
       } catch (error) {
@@ -21,7 +23,7 @@ const Admin = ({ user }) => {
         throw error;
       }
     };
-
+    console.log(endpoints);
     getAllStories();
   }, []);
 
@@ -32,7 +34,16 @@ const Admin = ({ user }) => {
   return (
     <main className="background flex flex-col gap-5">
       <Navbar pageNum={4} user={user} />
+
       <section className="flex h-full flex-1 w-full px-7 pb-5 items-center flex-col gap-3">
+        <section className="bg-white/70 font-bold rounded-md w-full max-w-5xl p-3 text-center uppercase text-xl text-midnight-green mt-4">
+          <h3 className="flex justify-center">All Endpoints</h3>
+        </section>
+        <section className="flex lg:grid-cols-3 max-w-5xl w-full gap-2 flex-col justify-center items-center md:grid-cols-2 md:grid md:justify-items-center">
+          {endpoints.map((endpoint) => (
+            <EndpointsCard key={endpoint.id} endpoint={endpoint} />
+          ))}
+        </section>
         <section className="bg-white/70 font-bold rounded-md w-full max-w-5xl p-3 text-center uppercase text-xl text-midnight-green mt-4">
           <h3 className="flex justify-center">All User Lores</h3>
         </section>
