612a044d7fd742f2b5fd1f1a62b695ee400ba760
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-25 21:53:46 -0800

    Reformatted the document

--- src/Pages/Utils/constants.jsx
+++ src/Pages/Utils/constants.jsx
@@ -19,11 +19,13 @@ export const NO_USER_STORIES = "There are no user lores in the database";
 export const ALL_USER_LORE_TEXT = "All User Lores";
 export const LOADING_TEXT = "Loading...";
 export const ERROR_GENERATING_TOKEN = "Error generating token:";
-export const CHARACTER_CONTEXT_CANT_BE_EMPTY = "Character context can't be empty";
+export const CHARACTER_CONTEXT_CANT_BE_EMPTY =
+  "Character context can't be empty";
 export const UNABLE_TO_GENERATE_STORY = "Unable to generate story.";
 export const ERROR_FETCHING_STORY = "Error fetching story:";
 export const STORY_RESULT_TEXT = "Story Result";
-export const DESCRIPTION_TEXT = "Telore crafts captivating fantasy backstories from your character context using artificial intelligence and storytelling magic. Try it out!";
+export const DESCRIPTION_TEXT =
+  "Telore crafts captivating fantasy backstories from your character context using artificial intelligence and storytelling magic. Try it out!";
 export const ERROR_OCCUR_TEXT = "An error occurred:";
 export const BACK_TEXT = "Back";
 export const HI_TEXT = "Hi";
@@ -31,14 +33,13 @@ export const REMAINING_REQUEST_TEXT = "Remaining Requests:";
 export const YOUR_LORES_TEXT = "Your Lores";
 export const YOUR_LORE_EMPTY_TEXT = "Your Lore Example";
 
-
 export const HTTP_METHODS = {
-    GET: 'GET',
-    POST: 'POST',
-    PATCH: 'PATCH',
-    DELETE: 'DELETE',
+  GET: "GET",
+  POST: "POST",
+  PATCH: "PATCH",
+  DELETE: "DELETE",
 };
 
 export const ENDPOINTS = {
-    SIGNOUT: '/signout',
+  SIGNOUT: "/signout",
 };
