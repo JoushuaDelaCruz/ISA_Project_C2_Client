d36c5712af11f11cf7068968596c2a06ec6c5003
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-25 20:50:34 -0800

    Linked the getAllUsers

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -10,12 +10,14 @@ import {
   ALL_USER_LORE_TEXT,
   LOADING_TEXT,
 } from "./Utils/constants";
+import UserCard from "./Components/UserCard";
 
 const Admin = ({ user }) => {
   const [loading, setLoading] = useState(true);
   const [lores, setLores] = useState([]);
   const { getRequest } = useRequest();
   const endpoints = useLoaderData();
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const getAllStories = async () => {
@@ -29,7 +31,17 @@ const Admin = ({ user }) => {
         throw error;
       }
     };
-    console.log(endpoints);
+    const getAllUsers = async () => {
+      try {
+        const endpoint = "/admin/users";
+        const users = await getRequest(endpoint);
+        setUsers(users);
+      } catch (error) {
+        console.error(ERROR_FETCHING_STORIES, error);
+        throw error;
+      }
+    };
+    getAllUsers();
     getAllStories();
   }, []);
 
@@ -51,19 +63,27 @@ const Admin = ({ user }) => {
           ))}
         </section>
         <section className="bg-white/70 font-bold rounded-md w-full max-w-5xl p-3 text-center uppercase text-xl text-midnight-green mt-4">
+          <h3 className="flex justify-center">All Users</h3>
+        </section>
+        <section className="flex lg:grid-cols-3 max-w-5xl w-full gap-2 flex-col justify-center items-center md:grid-cols-2 md:grid md:justify-items-center">
+          {users.map((user) => {
+            return <UserCard key={user.id} user={user} />;
+          })}
+        </section>
+        <section className="bg-white/70 font-bold rounded-md w-full max-w-5xl p-3 text-center uppercase text-xl text-midnight-green mt-4">
           <h3 className="flex justify-center">{ALL_USER_LORE_TEXT}</h3>
         </section>
         {loading ? (
           <p>{LOADING_TEXT}</p>
         ) : lores.length > 0 ? (
           <div className="max-w-5xl mx-auto">
-            <div className="grid grid-cols-3 gap-3">
-              {lores.map((lore, index) => (
-                <div key={index} className="flex justify-center">
-                  <div className="w-full max-w-md h-full">
-                    <StoryCardAdmin lore={lore} deleteLore={deleteLore} />
-                  </div>
-                </div>
+            <div className="flex lg:grid-cols-3 max-w-5xl w-full gap-2 flex-col justify-center items-center md:grid-cols-2 md:grid md:justify-items-center">
+              {lores.map((lore) => (
+                <StoryCardAdmin
+                  key={lore.id}
+                  lore={lore}
+                  deleteLore={deleteLore}
+                />
               ))}
             </div>
           </div>
